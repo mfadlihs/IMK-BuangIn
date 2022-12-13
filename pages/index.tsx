d6c517e0e2aa753e077872d61f4c67f69b8cfa5c
mfadlihs <winnerfadli999@gmail.com>
2022-12-13 12:46:07 +0700

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 import Home from "@/containers/Home";
+import Head from "next/head";
 
 export default function HomeRoutes() {
-	return <Home />;
+	// return <Home />;
+	return (
+		<>
+			<Head>
+				<title>BuangIn - Landing Page</title>
+			</Head>
+			<Home />
+		</>
+	);
 }
